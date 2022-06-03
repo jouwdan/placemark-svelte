70b051209e6d436d7f9186f0a59cc0d57cb30cc2
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2022-06-03 21:56:04 +0100

    Update Signup.svelte

--- src/pages/Signup.svelte
+++ src/pages/Signup.svelte
@@ -3,8 +3,14 @@
 
     import {push} from "svelte-spa-router";
 
+    let firstName = "";
+    let lastName = "";
     let email= "";
     let password = "";
+
+    async function signup() {
+        push("/");
+    }
   </script>
   
   <WelcomeMenu/>
@@ -15,17 +21,23 @@
   </div>
   <div class="flex justify-center p-12">
     <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <form action="/register" method="POST">
+      <form on:submit|preventDefault={signup}>
         <div class="card-body">
+            <div class="form-control">
+                <label for="firstname" class="label">First Name</label>
+                <input bind:value={firstName} id="firstname" class="input input-bordered w-full" type="text" placeholder="Enter first name" name="firstName">
+              </div>
+              <div class="form-control">
+                <label for="lastname" class="label">Last Name </label>
+                <input bind:value={lastName}  id="lastname" class="input input-bordered w-full" type="text" placeholder="Enter last name" name="lastName">
+              </div>
           <div class="form-control">
-            <label class="label" for="email">
+            <label class="label" for="email">Email Address</label>
                 <input bind:value={email}  class="input input-bordered w-full" id="email" name="email"placeholder="Enter Email" type="text">
-            </label>
           </div>
           <div class="form-control">
-            <label class="label" for="password">
-                <input bind:value={password}  class="input input-bordered w-full" id="password" name="password" placeholder="Enter Password" type="password">
-            </label>
+            <label class="label" for="password">Password</label>
+            <input bind:value={password}  class="input input-bordered w-full" id="password" name="password" placeholder="Enter Password" type="password">
           </div>
           <div class="form-control mt-6 justify-center">
             <button class="btn btn-neutral">Sign Up</button>
