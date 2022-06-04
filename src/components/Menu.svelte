7863e29dd846689b09314227dc2ad77dfa8aef28
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2022-06-04 15:30:51 +0100

    Update Menu.svelte

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -1,6 +1,6 @@
 <div class="navbar bg-opacity-90 w-full">
   <div class="flex-1">
-    <a class="btn btn-ghost normal-case text-xl" href="/">Placemark</a>
+    <a class="btn btn-ghost normal-case text-xl" href="/#/dashboard">Placemark</a>
   </div>
     <div class="flex-none">
       <ul class="menu menu-horizontal p-0">
