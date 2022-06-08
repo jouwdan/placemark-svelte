9c6c53ddb82564b7ba2beb6ea25fa44952910aeb
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2022-06-08 21:25:11 +0100

    white menu text

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -1,9 +1,9 @@
 <div class="navbar bg-opacity-90 w-full">
   <div class="flex-1">
-    <a class="btn btn-ghost normal-case text-xl" href="/#/dashboard">Placemark</a>
+    <a class="btn btn-ghost normal-case text-xl text-white" href="/#/dashboard">Placemark</a>
   </div>
     <div class="flex-none">
-      <ul class="menu menu-horizontal p-0">
+      <ul class="menu menu-horizontal p-0 text-white">
         <li><a href="/#/dashboard">Dashboard</a></li>
         <li><a href="/#/map">Map</a></li>
         <li><a href="/#/logout">Log Out</a></li>
--- src/components/WelcomeMenu.svelte
+++ src/components/WelcomeMenu.svelte
@@ -1,9 +1,9 @@
 <div class="navbar bg-opacity-90 w-full">
   <div class="flex-1">
-    <a class="btn btn-ghost normal-case text-xl" href="/">Placemark</a>
+    <a class="btn btn-ghost normal-case text-xl text-white" href="/">Placemark</a>
   </div>
   <div class="flex-none">
-    <ul class="menu menu-horizontal p-0">
+    <ul class="menu menu-horizontal p-0 text-white">
       <li><a href="/#/main">Home</a></li>
       <li><a href="/#/login">Log In</a></li>
       <li><a href="/#/signup">Sign Up</a></li>
