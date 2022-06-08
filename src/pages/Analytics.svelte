f5cb6493c30afafc372b2cbce2ecc153e141d562
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2022-06-08 22:30:32 +0100

    pie chart added

--- src/pages/Analytics.svelte
+++ src/pages/Analytics.svelte
@@ -1,16 +1,44 @@
 <script>
     import Menu from "../components/Menu.svelte";
+    import Chart from 'svelte-frappe-charts';
+    import {getContext, onMount} from "svelte";
+
+    const placemarkService = getContext("PlacemarkService");
+    let restaurantsList = [];((value, index) => data.indexOf(value) === index);
+
+    let cuisines = ["Irish", "Italian", "Chinese", "Indian", "Thai", "Mexican", "French", "Seafood", "Steakhouse", "Deli"];
+    
+    let cuisineChart = {
+        labels: cuisines,
+        datasets: [{
+            values: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        }]
+    };
+
+    onMount(async () => {
+        restaurantsList = await placemarkService.getRestaurants();
+        restaurantsList.forEach(restaurant => {
+            cuisines.forEach(cuisine => {
+                if(restaurant.cuisine == cuisine) {
+                    let cuisineIndex = cuisines.indexOf(cuisine);
+                    cuisineChart.datasets[0].values[cuisineIndex]++;
+                }
+            })
+        })
+        console.log(cuisineChart)
+    });
 </script>
 
 <Menu/>
 <div class="hero from-primary to-secondary text-primary-content -mt-[4rem] flex flex-col items-center bg-gradient-to-br p-16">
     <div class="hero-content w-full">
-      <h1 class="text-5xl font-bold p-12">Analytics</h1>
+      <h1 class="text-5xl font-bold p-12">Placemark Analytics</h1>
     </div>
   </div>
   
   <div class="container mx-auto mt-4">
     <div class="card bg-base-100 shadow-xl p-10">
-        Analytics go here
+        <h2 class="text-2xl mx-auto">Restaurants by Cuisine</h2>
+        <Chart data={cuisineChart} type="pie" maxSlices="10" />
     </div>
   </div>
